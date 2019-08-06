1bcd53d84e8256867b3668d21fab4589cc72c4d5
ckb\eckart <stephan.eckart@comhard.de>
2019-08-06 14:00:48 +0200

    Anwendungsrahmen implementiert

--- GitTestApp/DatabaseDOM/Program.cs
+++ GitTestApp/DatabaseDOM/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace DatabaseDOM
 {
@@ -10,6 +12,13 @@ namespace DatabaseDOM
     {
         static void Main(string[] args)
         {
+            DataSet set = new DataSet();
+            //den Adapter selbst konfigurieren
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            adapter.Fill(set);
+
+            //im Objektbaum navigieren und Daten ausgeben
         }
     }
 }
