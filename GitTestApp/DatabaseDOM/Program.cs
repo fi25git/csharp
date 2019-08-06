d11d05fae6ceb69e2a61ba78ac5fbcc8489179b9
ASperling <ASperling@ckb.comhard.de>
2019-08-06 14:07:42 +0200

    Vorschlag

--- GitTestApp/DatabaseDOM/Program.cs
+++ GitTestApp/DatabaseDOM/Program.cs
@@ -12,9 +12,22 @@ namespace DatabaseDOM
     {
         static void Main(string[] args)
         {
+            string host = "localhost";
+            int port = 3306;
+            string database = "test";
+            string username = "root";
+            string password = "";
+            String connString = "Server=" + host + ";Database=" + database
+                + ";port=" + port + ";User Id=" + username + ";password=" + password;
+
+            MySqlConnection conn = new MySqlConnection(connString);
+            conn.Open();
+
+
+
             DataSet set = new DataSet();
             //den Adapter selbst konfigurieren
-            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM film",conn);
 
             adapter.Fill(set);
 
